19e9ea4c5a09bac894d1422075f78863e0c0973d
mrwhoareyou <2359146115@qq.com>
2017-02-16 22:23:21 +0800

    relation

--- crawler.py
+++ crawler.py
@@ -1,19 +1,30 @@
 #coding=utf-8
 import urllib
 import urllib2
-import MySQLdb
-import config
+# import MySQLdb
+# import config
 import time
-import cookielib
+# import cookielib
 import random
-import re
-import socket
+# import re
+# import socket
 from bs4 import BeautifulSoup
-from pybloom import BloomFilter
+# from pybloom import BloomFilter
 
 class Crawler:
 	def __init__(self):	
-
+		USER_AGENT = '''Mozilla/5.0 (Windows NT 6.3; WOW64) 
+				AppleWebKit/537.36 (KHTML, like Gecko) 
+				Chrome/55.0.2883.87 Safari/537.36'''
+		headers = {    
+			'User-Agent':USER_AGENT,
+			'referer':'https://twitter.com/login'
+		}  
+		request = urllib2.Request("https://book.douban.com/people/meiyang/collect ", headers = headers)
+		response = urllib2.urlopen(request, timeout = 5)
+		pageHtml = response.read()
+		print pageHtml
+		return
 		#获取一个保存cookie的对象
 		cj = cookielib.LWPCookieJar()
 		#将一个保存cookie对象，和一个HTTP的cookie的处理器绑定
