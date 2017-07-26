fde4aded502919fb64c9765aa4c0e8c2d53b79e0
mrwhoareyou <2359146115@qq.com>
2017-07-26 18:33:04 +0800

    crawler

--- app/tweets_crawler.py
+++ app/tweets_crawler.py
@@ -6,11 +6,15 @@ from config import THREAD_NUM
 from twitter import error
 from api import Api, API_COUNT
 from database import MongoDB
-
+from decorator import generate_decorator
 
 class TweetsCrawler:
 	get_api = Api().get_api
-		
+	handle_exception = generate_decorator(300)
+
+	'''
+	获取用户推文信息，最多返回200条
+	'''
 	def get_user_timeline(self,
 						  user_id = None,
 						  screen_name = None, 
@@ -24,13 +28,21 @@ class TweetsCrawler:
 		if user_id == None and screen_name == None:
 			return None
 
-		return  self.get_api().GetUserTimeline(user_id = user_id,	screen_name = screen_name, 
-											   since_id = since_id, max_id = max_id, count = count,
-											   include_rts = include_rts, trim_user = trim_user,
+		return  self.get_api().GetUserTimeline(user_id = user_id,	
+											   screen_name = screen_name, 
+											   since_id = since_id, 
+											   max_id = max_id, 
+											   count = count,
+											   include_rts = include_rts, 
+											   trim_user = trim_user,
 											   exclude_replies = exclude_replies)
 
 
-	def get_user_all_timeline(user_id = None,
+	'''
+	获取用户所有推文信息，并保存在数据库中
+	'''
+	def get_user_all_timeline(self,
+							  user_id = None,
 						  	  screen_name = None, 
 						  	  collect_name = "tweets",
 						  	  include_rts = True, 
@@ -45,28 +57,35 @@ class TweetsCrawler:
 	
 		db = MongoDB().connect()
 		collect = db[collect_name]
-		api = self.api
+		get_api = self.get_api
 
 		while len(tweets) > 0:
 			try:
 				if flag:
-					tweets = get_api().GetUserTimeline(user_id = user_id, screen_name = screen_name, 
-													   include_rts = include_rts, exclude_replies = exclude_replies,
-							  	  					   trim_user = True, count = 200)
+					tweets = get_api().GetUserTimeline(user_id = user_id, 
+													   screen_name = screen_name, 
+													   include_rts = include_rts, 
+													   exclude_replies = exclude_replies,
+							  	  					   trim_user = True, 
+													   count = 200)
 					flag = False
 
 				else:
-					tweets = get_api().GetUserTimeline(user_id = user_id, screen_name = screen_name,
-													   include_rts = include_rts, exclude_replies = exclude_replies,
-							 						   trim_user = True, count = 200, max_id = tweets[-1].id - 1)
+					tweets = get_api().GetUserTimeline(user_id = user_id, 
+													   screen_name = screen_name,
+													   include_rts = include_rts, 
+													   exclude_replies = exclude_replies,
+							 						   trim_user = True, 
+													   count = 200, 
+													   max_id = tweets[-1].id - 1)
 
 			except error.TwitterError as te:
-				if te.message[0]['code'] == 88:
+				if hasattr(te.message, 'code') and te.message['code'] == 88:
 					sleep_count += 1
 					if sleep_count == API_COUNT:
 						print "sleeping..."
 						sleep_count = 0
-						time.sleep(600)
+						time.sleep(300)
 					continue
 				else:
 					break
@@ -74,38 +93,85 @@ class TweetsCrawler:
 				break
 				
 			for tt in tweets:
-				tweet = {
-					'coordinates': tt.coordinates,  # Coordinates
-					'created_at': tt.created_at, # String
-					'favorite_count': tt.favorite_count, # int
-					'filter_level': tt.filter_level if hasattr(tt, 'filter_level') else '', # String
-					'hashtags': map(lambda x: x.text, tt.hashtags), # {'0': ,'1':}
-					'_id': tt.id_str, # String
-					# 'in_reply_to_screen_name': tt.in_reply_to_screen_name,
-					'in_reply_to_status_id': tt.in_reply_to_status_id,
-					'in_reply_to_user_id': tt.in_reply_to_user_id,
-					'lang': tt.lang, # String
-					# 'media': tt.media,
-					'place': tt.place, # Place
-					'possibly_sensitive': tt.possibly_sensitive, # Boolean
-					'retweet_count': tt.retweet_count, # int
-					'source': tt.source, # String
-					'text': tt.text, # String
-					# 'truncated': tt.truncated,
-					# 'urls': tt.urls, # []
-					'user_id': tt.user.id, # int
-					'user_mentions': map(lambda x: x.id, tt.user_mentions), # []
-					'withheld_copyright': tt.withheld_copyright, # Boolean
-					'withheld_in_countries': tt.withheld_in_countries, # Array of String
-					'withheld_scope': tt.withheld_scope, #String
-				}
+				tweet = self.tweetobj_to_dict(tt)
 				try:
 					collect.insert_one(tweet)
 				except Exception as e:
 					continue
 	
 
-	def get_all_users_timeline(user_list = None,
+	'''
+	获取用户所有推文信息，并返回
+	'''
+	def get_user_all_timeline_return(self, 
+								     user_id = None,
+							  	     screen_name = None, 
+							  	     include_rts = True, 
+							  	     exclude_replies = False):
+
+		if user_id == None and screen_name == None:
+			return None
+
+		if user_id:
+			user_id = long(user_id)
+
+		flag = True
+		tweets = [0]
+		sleep_count = 0
+
+		tweet_list = []
+
+		get_api = self.get_api
+
+		while len(tweets) > 0:
+			try:
+				if flag:
+					tweets = get_api().GetUserTimeline(user_id = user_id, 
+													   screen_name = screen_name, 
+													   include_rts = include_rts, 
+													   exclude_replies = exclude_replies,
+								  	  				   trim_user = True, 
+													   count = 200)
+					flag = False
+
+				else:
+					tweets = get_api().GetUserTimeline(user_id = user_id, 
+													   screen_name = screen_name,
+												   	   include_rts = include_rts, 
+													   exclude_replies = exclude_replies,
+							 					   	   trim_user = True, 
+													   count = 200, 
+													   max_id = tweets[-1].id - 1)
+
+			except error.TwitterError as te:
+				if hasattr(te.message, 'code') and te.message['code'] == 88:
+					sleep_count += 1
+					if sleep_count == API_COUNT:
+						print "sleeping..."
+						sleep_count = 0
+						time.sleep(300)
+					continue
+				else:
+					break
+			except Exception as e:
+				print e
+				break
+
+			for tt in tweets:
+				tweet = self.tweetobj_to_dict(tt)
+				try:
+					tweet_list.append(tweet)
+				except Exception as e:
+					continue
+
+		return tweet_list
+
+
+	'''
+	获取所有用户推文信息
+	'''
+	def get_all_users_timeline(self,
+							   user_list = None,
 							   collect_name = "tweets",
 							   search_type = "user_id",
 							   include_rts = True, 
@@ -131,27 +197,62 @@ class TweetsCrawler:
 
 			i += 1
 
-		for thread in thread_pool:
-			thread.join()
+		for t in thread_pool:
+			t.join()
 
 
+	'''
+	线程：获取多个用户推文信息
+	'''
 	def get_users_timeline_thread(self,
 								  user_list = [], 
 								  collect_name = "tweets", 
 								  search_type = "user_id", 
 								  include_rts = True, 
 								  exclude_replies = False):
-	
-		while len(user_list) > 0:
-			user_id = user_list.pop(0)
 
-			if search_type == "screen_name":
-				self.get_user_all_timeline(screen_name = user_id,
-									       collect_name = collect_name,
-						  	  		       include_rts = include_rts, 
-						  	  	           exclude_replies = exclude_replies)
-			else:
+		if search_type != "screen_name":
+			while len(user_list) > 0:
+				user_id = user_list.pop(0)
+
 				self.get_user_all_timeline(user_id = user_id,
 									       collect_name = collect_name,
 						  	  		       include_rts = include_rts, 
 						  	  	           exclude_replies = exclude_replies)
+		else:
+			while len(user_list) > 0:
+				screen_name = user_list.pop(0)
+
+				self.get_user_all_timeline(screen_name = screen_name,
+									       collect_name = collect_name,
+						  	  		       include_rts = include_rts, 
+						  	  	           exclude_replies = exclude_replies)
+
+
+	'''
+	将推文对象转换为字典类型
+	'''
+	def tweetobj_to_dict(self, tt):
+		tweet = {
+			'coordinates': tt.coordinates,  # Coordinates
+			'created_at': tt.created_at, # String
+			'favorite_count': tt.favorite_count, # int
+			'filter_level': tt.filter_level if hasattr(tt, 'filter_level') else '', # String
+			'hashtags': map(lambda x: x.text, tt.hashtags), # {'0': ,'1':}
+			'_id': tt.id_str, # String
+			'in_reply_to_status_id': tt.in_reply_to_status_id,
+			'in_reply_to_user_id': tt.in_reply_to_user_id,
+			'lang': tt.lang, # String
+			'place': tt.place, # Place
+			'possibly_sensitive': tt.possibly_sensitive, # Boolean
+			'retweet_count': tt.retweet_count, # int
+			'source': tt.source, # String
+			'text': tt.text, # String
+			'user_id': tt.user.id, # int
+			'user_mentions': map(lambda x: x.id, tt.user_mentions), # []
+			'withheld_copyright': tt.withheld_copyright, # Boolean
+			'withheld_in_countries': tt.withheld_in_countries, # Array of String
+			'withheld_scope': tt.withheld_scope, #String
+		}
+
+		return tweet
--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 import threading
 
-from app.database import Mysql
+from app.database import Mysql, MongoDB
 from app.basicinfo_crawler import BasicinfoCrawler
 from app.tweets_crawler import TweetsCrawler
 from app.relation_crawler import RelationCrawler
@@ -9,10 +10,15 @@ basicinfo_crawler = BasicinfoCrawler()
 tweets_crawler = TweetsCrawler()
 relation_crawler = RelationCrawler()
 
+'''
+获取所有用户的基础信息，存到 table_name 中
+'''
 def get_users_basicinfo(user_list, table_name = "user", search_type = "user_id"):
 	basicinfo_crawler.get_all_users(user_list, table_name = table_name, search_type = search_type)
 
-
+'''
+读取文件中的用户，抓取基础信息，存放到 table_name 中
+'''
 def get_users_basicinfo_from_file(file_name, table_name = "user", search_type = "screen_name"):
 	file = open(file_name)
 	user_list = []
@@ -26,7 +32,9 @@ def get_users_basicinfo_from_file(file_name, table_name = "user", search_type = 
 
 	basicinfo_crawler.get_all_users(user_list, table_name = table_name, search_type = search_type)
 
-
+'''
+读取数据库中的用户，抓取基础信息，存放到 table_name 中
+'''
 def get_users_basicinfo_from_db(sql, table_name = "user", search_type = "user_id"):
 	mysql = Mysql()
 	mysql.connect()
@@ -40,11 +48,16 @@ def get_users_basicinfo_from_db(sql, table_name = "user", search_type = "user_id
 
 	basicinfo_crawler.get_all_users(user_list, table_name = table_name, search_type = search_type)
 
-
+'''
+获取所有用户的推文信息，存放到 collect_name 中
+'''
 def get_users_tweets(user_list, collect_name = "tweets", search_type = "user_id"):
 	tweets_crawler.get_all_users_timeline(user_list, collect_name = collect_name, search_type = search_type)
 
 
+'''
+读取文件中的用户，抓取推文信息，存放到 collect_name 中
+'''
 def get_users_tweets_from_file(file_name, collect_name = "tweets", search_type = "screen_name"):
 	file = open(file_name)
 	user_list = []
@@ -58,7 +71,9 @@ def get_users_tweets_from_file(file_name, collect_name = "tweets", search_type =
 
 	tweets_crawler.get_all_users_timeline(user_list, collect_name = collect_name, search_type = search_type)
 
-
+'''
+读取数据库中的用户，抓取推文信息，存放到 collect_name 中
+'''
 def get_users_tweets_from_db(sql, collect_name = "tweets", search_type = "screen_name"):
 	mysql = Mysql()
 	mysql.connect()
@@ -72,7 +87,9 @@ def get_users_tweets_from_db(sql, collect_name = "tweets", search_type = "screen
 
 	tweets_crawler.get_all_users_timeline(user_list, collect_name = collect_name, search_type = search_type)
 
-
+'''
+获取人物关系
+'''
 def get_users_relationship(user_list):
 	mysql = Mysql()
 	mysql.connect()
@@ -124,8 +141,6 @@ def get_users_relation_thread(user_list, all_users, start_index, s):
 	mysql = Mysql()
 	mysql.connect()
 
-	# print str(len(user_list)) + "|" + str(len(all_users)) + "|" + str(start_index) + "**"
-
 	i = 0
 	for i in range(length):
 		for j in range(start_index + i + 1, all_length):
@@ -152,18 +167,45 @@ def get_users_relation_thread(user_list, all_users, start_index, s):
 				print e
 				continue
 
+
+def filter():
+	db = MongoDB().connect()
+	collect = db['typical']
+
+	users = collect.find()
+
+	res = []
+	for item in users:
+		u = basicinfo_crawler.get_user(user_id = item['_id'])
+		collect.update({"_id": item['_id']}, {'$set': {'created_at': u.created_at, 'lang': u.lang, 
+			'protected': u.protected, 'time_zone': u.time_zone, 'utc_offset': u.utc_offset,
+			'listed_count': u.listed_count, 'default_profile_image': u.default_profile_image, 'profile_background_color': u.profile_background_color,
+			'profile_image_url': u.profile_image_url, 'profile_banner_url': u.profile_banner_url}})
+
+	
+
 		
 if __name__ == "__main__":
-	mysql = Mysql()
-	mysql.connect()
+	# mysql = Mysql()
+	# mysql.connect()
 
-	sql = "select userid from standardusers"
+	# sql = "select userid from standardusers"
 
-	try:
-		user_list = mysql.fetchall(sql)
-	except Exception as e:
-		print e
+	# try:
+	# 	user_list = mysql.fetchall(sql)
+	# except Exception as e:
+	# 	print e
 
-	user_list = map(lambda x: x[0], user_list)
+	# user_list = map(lambda x: x[0], user_list)
 
-	get_users_relationship(user_list)
+	# get_users_relationship(user_list)
+
+	db = MongoDB().connect()
+	collect = db['typical']
+
+	users = collect.find({}, {'_id': 1})
+
+	for u in users:
+		print u['_id']
+
+	
